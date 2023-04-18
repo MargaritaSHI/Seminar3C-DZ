9eead48f18bd6141acdaaffdb42fcd442d8404bb
Margarita <diasimba2014@gmail.com>
2023-04-18 22:07:21 +0300

    папка 4го семинара

---
+++ CX/Program.cs
@@ -0,0 +1,30 @@
+﻿int number = GetUserNumber("Введите число: ","Ошибка");
+int result = CountDidgits(number);
+System.Console.WriteLine(result);
+
+#region 
+int CountDidgits(int number)
+{
+    int count = 0;
+    while (number != 0)
+    {
+        count++;
+        number /= 10;
+    }
+    return count;
+}
+
+int GetUserNumber(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        int userInput;
+        if (int.TryParse(Console.ReadLine(), out userInput))
+        {
+            return userInput;
+        }
+        else Console.WriteLine(errorMessage);
+    }
+}
+#endregion
---
+++ Zadacha21/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние
+// между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+int x1 = GetUserNumber("Введите значение точки А x1: ");
+int y1 = GetUserNumber("Введите значение точки А y1: ");
+int z1 = GetUserNumber("Введите значение точки А z1: ");
+int x2 = GetUserNumber("Введите значение точки В х1: ");
+int y2 = GetUserNumber("Введите значение точки В y1: ");
+int z2 = GetUserNumber("Введите значение точки D z1: ");
+
+double result = Math.Sqrt (Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine($"a ({x1},{y1},{z1}); b ({x2},{y2},{z2}) -> {result:f2}");
+
+static int GetUserNumber(string message)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        int userImput;
+        if (int.TryParse(Console.Readline(), out userImput))
+        {
+            return (userImput);
+        }
+        else Console.WriteLine("Введены некорректные данные"); 
+    }
+}
+    
+    
